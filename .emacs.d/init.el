c807a6b9f1635b67fe83aac50f1c67011908b237
Magnus Bohman <magnus.bohman@gmail.com>
2016-10-05 21:49:28 +0200

    initial commit version 2 of mydotemacs
    Restart of my emacs config. This time around,
    K.I.S.S will be followed, and new things
    will only be added once need for it appear.

---
+++ .emacs.d/init.el
@@ -0,0 +1,13 @@
+; init.el
+; Main init file for my settings 
+
+(defconst *user-home-directory*
+  (or (getenv "HOME")
+      (expand-file-name ".." user-emacs-directory))
+  "Home sweet home directory.")
+
+; Adds our lisp dir to load path
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;Initiate global key bindings
+(require 'global-bindings)
---
+++ .emacs.d/lisp/global-bindings.el
@@ -0,0 +1,8 @@
+;; global-bindings.el
+;; This file define all key bindings for "always on" modes and other emacs tweaks
+
+; Change C-x C-m to C-x m
+(global-set-key (kbd "C-x C-m") 'execute-extended-command)
+(global-set-key (kbd "C-x m") 'execute-extended-command)
+
+(provide 'global-bindings)
