8daa69517157baa2c796f862f0c1016baf463ccf
Magnus Bohman <magnus.bohman@gmail.com>
2016-10-08 19:03:46 +0200

    adds basic C/C++ mode

--- .emacs.d/lisp/modes.el
+++ .emacs.d/lisp/modes.el
@@ -2,5 +2,6 @@
 ;; Loads various additional modes
 
 (require 'mode/lisp "modes/lisp.el")
+(require 'mode/c-c++ "modes/c-c++.el")
 
 (provide 'modes)
---
+++ .emacs.d/lisp/modes/c-c++.el
@@ -0,0 +1,24 @@
+;; c-c++.el
+; setup up C/C++ mode 
+(defun my/c-c++-mode-hook()
+
+  (setq c-basic-offset 4)
+  (setq-default indent-tabs-mode nil)
+
+  (setq-default c-default-style "K&R")
+  (setq-default c++-style "Stroustrup")
+  (c-set-offset 'substatement-open 0)
+
+  (subword-mode t)
+
+  (require 'mic-paren)
+  (paren-activate)
+)
+
+; Treat all .h, .hpp files as c++
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
+
+(add-hook 'c-mode-common-hook 'my/c-c++-mode-hook)
+
+(provide 'mode/c-c++)
