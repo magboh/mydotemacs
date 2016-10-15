199522cf31a9f2bbd163a55139c5462b55b8c6e8
Magnus Bohman <magnus.bohman@gmail.com>
2016-10-15 12:11:48 +0200

    adds rtags to company
    Requires company-rtags.el existings in ~/.emacs.d/

--- .emacs.d/lisp/modes/company.el
+++ .emacs.d/lisp/modes/company.el
@@ -9,4 +9,11 @@
   (setq company-idle-delay .4)
   (setq company-echo-delay 0))
 
+(defun my/init-company-rtags ()
+  (my/init-company)
+  (require 'rtags)
+  (load-file "~/.emacs.d/company-rtags.el")
+  (setq company-bcankends '(company-rtags))
+)
+
 (provide 'mode/company)
--- .emacs.d/lisp/packages.el
+++ .emacs.d/lisp/packages.el
@@ -21,6 +21,7 @@
 		      magit
 		      mic-paren
                       projectile
+                      rtags
                       ))
 
 (dolist (p my-packages)
